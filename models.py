ab3cb32517fe108243ac54b098ca0787b06dc72b
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-05-29 12:41:02 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ category = models.CharField(max_length=100)
 description = models.TextField(blank=True)
 price = models.DecimalField(max_digits=10, decimal_places=2)
 
-```
+
 def __str__(self):
     return self.name
-```
+
